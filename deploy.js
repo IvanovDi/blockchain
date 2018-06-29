c23f8d29fd6084e644910b36f204ea804769bf4d
Dima <dima77703011983@gmail.com>
2018-06-29 15:12:27 +0300

    Add new logic in deploy file

--- deploy.js
+++ deploy.js
@@ -9,3 +9,20 @@ const provider = new HDWalletProvider(
 );
 
 const web3 = new Web3(provider);
+
+const deploy = async () => {
+    try {
+        const accounts = await web3.eth.getAccounts();
+
+        console.log('Attempting to deploy from account', accounts[0]);
+        const result = await new web3.eth.Contract(JSON.parse(interface))
+            .deploy({ data: bytecode, arguments: ['Hi there!'] })
+            .send({ from: accounts[0], gas: '1000000' });
+        console.log('Contract deployed to', result.options.address);
+
+    } catch (e) {
+        console.log(e.message);
+    }
+    return 0;
+};
+deploy();
